eda3ed8bda7ceb427574370cf9a0db8e6f0db102
allagod <1416527409@qq.com>
2023-12-21 22:47:24 +0800

    PLay类草稿

---
+++ Classes/Player.cpp
@@ -0,0 +1,54 @@
+#include "Player.h"
+
+
+
+void Player::intoBoardCode(char* tmp, ChessIfo now)
+{
+	if (now.type == NONE_CHESS)
+	{
+		for (int i = 0; i < 5; i++)
+			tmp[i] = '0';
+		return;
+	}
+	tmp[0] = now.type; tmp[1] = now.level + '0';
+	for (int i = 2; i < 5; i++)
+	{
+		tmp[i] = now.equipment[i - 2] + '0';
+	}
+}
+
+// 该函数返回要参与战斗的棋子的位置信息
+// %开头，用五位字符表示一个位置的棋子
+// 第一位是棋子的类型，第二位是棋子的星级，第三到五位表示棋子所携带的装备
+char* Player::getBoardCode()
+{
+	char tmp[150];
+	tmp[0] = '%';
+	for (int i = 1; i <= 4; i++)
+		for (int j = 1; j <= 7; j++)
+			intoBoardCode(tmp + 1 + (i * 7 + j - 8) * 5, BoardChessIfo[i][j]);
+	tmp[1 + (4 * 7 + 8 - 8) * 5] = '0';
+	return tmp;
+} 
+
+// 往棋盘中放棋子
+void Player::intoChess(char now_type, int now_level, int a, int b, int c, int x, int y)
+{
+	BoardChessIfo[x][y].type = now_type;
+	BoardChessIfo[x][y].level = now_level;
+	BoardChessIfo[x][y].equipment[0] = a;
+	BoardChessIfo[x][y].equipment[1] = b;
+	BoardChessIfo[x][y].equipment[2] = c;
+	return;
+}
+
+// 将(x, y) 处的棋子清空
+void Player::clearChess(int x, int y)
+{
+	BoardChessIfo[x][y].type = NONE_CHESS;
+	BoardChessIfo[x][y].level = 0;
+	BoardChessIfo[x][y].equipment[0] = 0;
+	BoardChessIfo[x][y].equipment[1] = 0;
+	BoardChessIfo[x][y].equipment[2] = 0;
+	return;
+}
--- Classes/Player.h
+++ Classes/Player.h
@@ -1,1 +1,29 @@
 //用于存储玩家的小小英雄，血量，等级等信息
+#include <iostream>
+using namespace std;
+
+class Player
+{
+private:
+#define NONE_CHESS 0
+	int health = 0, level = 0;
+	struct ChessIfo
+	{
+		char type = NONE_CHESS;
+		int level = 0;
+		int equipment[3] = { 0 };
+	};
+	ChessIfo BoardChessIfo[6][12];
+	void intoBoardCode();
+public:
+	char* getBoardCode();
+	void intoChess(char now_type, int now_level, int a, int b, int c, int x, int y); // 往棋盘中放棋子
+	void clearChess(int x, int y); // 将(x, y) 处的棋子清空
+
+	// 接口函数
+	int getHealth() { return health; }
+	int getLevel() { return level; }
+	void levelUp() { level++; }
+	void healthDown(int down) { health -= down; }
+	void healthUp(int up) { health += up; }
+};
