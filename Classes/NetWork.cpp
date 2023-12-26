6416424338c45a2a80c74e288cd0532c07354bd0
allagod <1416527409@qq.com>
2023-12-26 20:24:03 +0800

    Network更新

--- Classes/NetWork.cpp
+++ Classes/NetWork.cpp
@@ -1,6 +1,20 @@
-#include "NetWork"
+#include "Network.h"
+#include <thread>
+#include <cstring>
+using namespace std;
 
-NetWork::NetWork()
+Network::Network()
+{
+	init();
+}
+
+Network* Network::getInstance()
+{
+	static Network local_instance;
+	return &local_instance;
+}
+
+void Network::init()
 {
 	//1. 确定网络协议版本		
 	WSADATA wsaData;
@@ -27,7 +41,7 @@ NetWork::NetWork()
 	//3. 确定服务器协议地址簇	
 	SOCKADDR_IN  addr = { 0 };
 	addr.sin_family = AF_INET;
-	addr.sin_addr.S_un.S_addr = inet_addr("100.79.182.197");
+	addr.sin_addr.S_un.S_addr = inet_addr("100.81.181.249");
 	addr.sin_port = htons(13526);
 
 	//4. 连接服务器
@@ -43,7 +57,11 @@ NetWork::NetWork()
 	printf("连接服务器成功\n");
 }
 
-NetWork::~NetWork()
+Network::Network()
+{
+}
+
+Network::~Network()
 {
 	//8. 关闭socket		
 	closesocket(serverSocket);
@@ -51,7 +69,7 @@ NetWork::~NetWork()
 	WSACleanup();
 }
 
-void NetWork::sendNikename(char a[])
+void Network::sendNikename(char a[])
 {
 	strcpy_s(nikename, a);
 	char tmp[MAX_NIKELEN * 4] = "*";
@@ -59,7 +77,7 @@ void NetWork::sendNikename(char a[])
 	send(serverSocket, tmp, strlen(tmp), NULL);
 }
 
-char* NetWork::refresh()
+char* Network::refresh()
 {
 	send(serverSocket, "#", strlen("#"), NULL);
 	int r;
@@ -72,7 +90,7 @@ char* NetWork::refresh()
 	}
 }
 
-int NetWork::buildRoom()
+int Network::buildRoom()
 {
 	send(serverSocket, "@", strlen("@"), NULL);
 	int r;
@@ -88,7 +106,7 @@ int NetWork::buildRoom()
 	}
 }
 
-bool NetWork::intoRoom(int index)
+bool Network::intoRoom(int index)
 {
 	char tmp[5];
 	tmp[0] = '!'; tmp[1] = index + '0'; tmp[2] = 0;
@@ -105,10 +123,11 @@ bool NetWork::intoRoom(int index)
 	}
 }
 
-void NetWork::wait()
+void Network::wait()
 {
 	int r;
-	char buff[BUFF_LEN], gameStart[] = "GameStart";
+	memset(buff, 0, sizeof(buff));
+	char gameStart[] = "GameStart";
 	while (1)
 	{
 		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
@@ -119,7 +138,106 @@ void NetWork::wait()
 	send(serverSocket, "&", strlen("&"), NULL);
 }
 
-void NetWork::startGame()
+void Network::startGame()
 {
+	isHost = 1;
 	send(serverSocket, "&&", strlen("&&"), NULL);
+	int r;
+	while (1)
+	{
+		memset(buff, 0, sizeof(buff));
+		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
+		if (r > 0)
+		{
+			cout << "startGameBuff:" << buff << endl;
+			if (strcmp(buff, "GameStart") == 0)
+				break;
+		}
+	}
+}
+
+char* Network::getNextEnemy()
+{
+	cout << "successingetNextEnemy\n";
+	if (isHost)
+	{
+		cout << "getNextEnemySend:" << "=" << endl;
+		send(serverSocket, "=", strlen("="), NULL);
+	}
+	memset(buff, 0, sizeof(buff));
+	int r;
+	while (1)
+	{
+		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
+		if (r > 0)
+			return buff;
+	}
+}
+
+char* Network::getFightBoard(const char myBoard[])
+{
+	if (isHost)
+		send(serverSocket, "+", strlen("+"), NULL);
+	int r;
+	while (1)
+	{
+		memset(buff, 0, sizeof(buff));
+		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
+		if (r > 0)
+			if (strcmp(buff, "readyToFight") == 0)
+				break;
+	}
+	send(serverSocket, myBoard, strlen(myBoard), NULL);
+	if (isHost)
+		send(serverSocket, "$", strlen("$"), NULL);
+	while (1)
+	{
+		memset(buff, 0, sizeof(buff));
+		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
+		if (r > 0)
+			if (buff[0] == '%')
+				break;
+	}
+	return buff;
+}
+
+void Network::fightOver(int tag)
+{
+	memset(buff, 0, sizeof(buff));
+	buff[0] = 'd';
+	if (tag)
+		buff[1] = 'd';
+	send(serverSocket, buff, strlen(buff), NULL);
+	int r;
+	while (1)
+	{
+		memset(buff, 0, sizeof(buff));
+		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
+		if (r > 0)
+			if (strcmp(buff, "goBuy") == 0)
+				break;
+	}
+}
+
+char* Network::getCard(int level)
+{
+	memset(buff, 0, sizeof(buff));
+	buff[0] = 'm'; buff[1] = level + '0'; buff[2] = 0;
+	send(serverSocket, buff, strlen(buff), NULL);
+	int r;
+	while (1)
+	{
+		memset(buff, 0, sizeof(buff));
+		r = recv(serverSocket, buff, BUFF_LEN - 1, NULL);
+		if (r > 0)
+			break;
+	}
+	return buff;
+}
+
+void Network::cardBack(char ch, int num)
+{
+	memset(buff, 0, sizeof(buff));
+	buff[0] = '-'; buff[1] = ch; buff[2] = num + '0'; buff[3] = 0;
+	send(serverSocket, buff, strlen(buff), NULL);
 }
--- Classes/NetWork.h
+++ Classes/NetWork.h
@@ -2,27 +2,35 @@
 #include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
+class Network
+{
+private:
 #define MAX_NIKELEN 64
 #define BUFF_LEN 64
 #define MAX_NIKELEN 64
+	Network();
+	Network(const Network&) = delete;
+	Network& operator =(const Network&) = delete;
 
-class NetWork
-{
-private:
+
 	char nikename[MAX_NIKELEN]; //存储玩家昵称
 	SOCKET serverSocket; //不用管
 	char buff[BUFF_LEN];//不用管
+	int isHost = 0; //是否是房主
 public:
-	NetWork();
-	~NetWork();
-	void sendNikename(char nikename[]); //输入昵称后用这个函数把昵称发给服务器
+	static Network* getInstance();
+	//初始化函数v
+	void init();
+
+	//输入昵称后用这个函数把昵称发给服务器
+	void sendNikename(char nikename[]);
 
 	//如果在大厅，返回各个房间的信息
 	//例如"3#1guolinghao##2yg##5chl#"表示共三个房间，各个房间的房主是#...#，房间序号为#后的数字
 
 	//如果在房间内，返回房间内的信息
 	//"2#guolinghao##yg#"第一个字符表示房间内有多少人，#..#中间是房间内玩家昵称
-	char* refresh(); 
+	char* refresh();
 
 	//创建房间，如果成功返回创建房间的序号，失败返回-1
 	int buildRoom();
@@ -35,4 +43,19 @@ public:
 
 	//房主开始游戏调用该函数
 	void startGame();
+
+	//返回下一局可能对战谁
+	char* getNextEnemy();
+
+	//传入自己的棋盘，返回对手的棋盘
+	char* getFightBoard(const char myBoard[]);
+
+	//传入是否死亡，0代表没死，1代表死亡
+	void fightOver(int die);
+
+	//返回商城给的卡
+	char* getCard(int level);
+
+	//将没买的卡退回去
+	void cardBack(char ch, int num);
 };
